cd37a0d5d4702d94a0c7e853e039df5d4b4e147a
User <User@5114001660TI778>
2022-10-16 19:26:37 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_DeclareString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q1：顯示 c:\temp\a.jpg
+            Console.WriteLine("D:\\temp\\animal.jpg");
+            Console.WriteLine(@"D:\temp\animal.jpg");
+
+            Console.WriteLine();
+
+            //Q2：顯示 我是一個"好學生".
+            Console.WriteLine("我是一個\"好學生\".");
+
+            Console.WriteLine();
+
+            //Q3：顯示兩行文字
+            //我是第一行
+            //我是第二行
+            Console.WriteLine("我是第一行\n我是第二行");
+
+            Console.Read();
+        }
+    }
+}
